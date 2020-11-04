5310ae15f6e0a961e0ebb19099a8ac5eda3d1f0b
micnem <michael.nemni@gmail.com>
2020-11-04 11:23:15 +0200

    changes to q1

--- q1/q1.js
+++ q1/q1.js
@@ -1,1 +1,9 @@
-console.log("hello")
+//list of cities that are draggable
+let cities = document.getElementsByClassName("city")
+for(let city of cities){
+    city.setAttribute("draggable", "true");
+    // city.setAttribute("ondragstart", "dragStart(event)");
+    // city.setAttribute("ondragend", "dragEnd(event)");
+}
+//map of Israel 
+//data data-target and data-accept 
